a9153ab75364dfdb11f9b2710d8f2cc6f29e7315
mm <mm@mm-PC>
2021-03-28 14:09:30 +0900

    lombok 설치완료

--- study/src/main/java/com/example/demo/model/SearchParam.java
+++ study/src/main/java/com/example/demo/model/SearchParam.java
@@ -1,29 +1,15 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data //기본 생성자와 get set를 만들어줌..
+@AllArgsConstructor // 모든 변수를 가진 생성자 추가
 public class SearchParam {
+	
 	
 	private String account;
 	private String email;
 	private int page;
-	
-	public String getAccount() {
-		return account;
-	}
-	public void setAccount(String account) {
-		this.account = account;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public int getPage() {
-		return page;
-	}
-	public void setPage(int page) {
-		this.page = page;
-	}
 	
 	
 }
