a91fc6de5f8fe71f7decdcdf2db38ec28dba22b5
mm <mm@mm-PC>
2021-03-28 16:51:53 +0900

    mysql 연결과 gradle,application.properties 설정 추가

--- study/src/main/java/com/example/demo/controller/postController.java
+++ study/src/main/java/com/example/demo/controller/postController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
