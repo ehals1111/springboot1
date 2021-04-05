3bd4263afb06ce86de242ee05cdf3afc0dae2abc
mm <mm@mm-PC>
2021-04-05 20:33:31 +0900

    JPA CRUD_2 UPDATE() 완료

--- study/src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ study/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -2,10 +2,15 @@ package com.example.demo.repository;
 
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.example.demo.StudyApplicationTests;
 import com.example.demo.model.User;
+
 
 
 public class UserRepositoryTest extends StudyApplicationTests{
@@ -31,11 +36,31 @@ public class UserRepositoryTest extends StudyApplicationTests{
 		System.out.println("newUser "+newUser);
 	}
 	
+	@Test
 	public void read() {
+		Optional<User> user =userRepository.findById(2L);
 		
+		//ifPresent 있을떼만 값을 ..
+		user.ifPresent(selectUser ->{
+			System.out.println("user: "+selectUser);
+			System.out.println("user: "+selectUser.getEmail());
+			}
+		);
 	}
 	
-	public void updaate() {
+	public void update() {
+		Optional<User> user =userRepository.findById(2L); //특정 유저를 셀렉트 해준다 그래야 특정유저를 수정할수 있기때문에
+		user.ifPresent(selectUser ->{
+			selectUser.setAccount("pppp");
+			selectUser.setUpdatedAt(LocalDateTime.now());
+			selectUser.setUpdatedBy("updatge method()");
+			
+			userRepository.save(selectUser);
+			}
+		);
+	}
+	
+	public void delete() {
 		
 	}
 }
