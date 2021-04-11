a93ad214e73b68bc978aebed5cbe73d60d462bde
mm <mm@mm-PC>
2021-04-11 14:56:43 +0900

    상품 api create 1

---
+++ study/src/main/java/com/example/demo/controller/api/ItemApiController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller.api;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.ifs.CrudInterface;
+import com.example.demo.network.Header;
+import com.example.demo.network.request.ItemApiRequest;
+import com.example.demo.network.response.ItemApiResponse;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse>{
+
+	@Override
+	@PostMapping("") // /api/item
+	public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@GetMapping("{id}") // /api/item/1
+	public Header<ItemApiResponse> read(@PathVariable Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@PutMapping("") // /api/item
+	public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@DeleteMapping("{id}") // /api/item/1
+	public Header delete(@PathVariable Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+		
+}
